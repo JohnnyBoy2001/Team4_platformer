43f87f92ed217455a1629999c3e063de388ce2ca
DESKTOP-OUJS0N1\John <johnnytowwez@gmail.com>
2020-10-01 14:50:52 -0400

    Added ability for enemies to move.

---
+++ Assets/Scripts/EnemyAgro.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class EnemyAgro : MonoBehaviour
+{
+    public GameObject Player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(Player.transform);
+        transform.position += transform.forward * 1f * Time.deltaTime;
+    }
+}
