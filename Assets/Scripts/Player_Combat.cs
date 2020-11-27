2d0f7274f79010bb13c81687a0418acbec1fcbde
DESKTOP-OUJS0N1\John <johnnytowwez@gmail.com>
2020-11-27 15:56:29 -0500

    Premetaive Melee System (Need death animations and attack animations)

---
+++ Assets/Scripts/Player_Combat.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Combat : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator animator;
+    public LayerMask enemyLayers;
+    public Transform AttackPoint;
+
+    public float attackRange = 0.5f;
+    public int attackDamage = 20;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Attack();
+        }
+    }
+    void Attack()
+    {
+        //Play Animation
+        animator.SetTrigger("Attack");
+
+        //Detect Enemies
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(AttackPoint.position, attackRange, enemyLayers);
+
+        //Damage Enemies
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<EnemyHealth>().TakeDamage(attackDamage);
+        }
+    }
+    void OnDrawGizmosSelected()
+    {
+        if (AttackPoint == null)
+            return;
+
+        Gizmos.DrawWireSphere(AttackPoint.position, attackRange);
+    }
+}
