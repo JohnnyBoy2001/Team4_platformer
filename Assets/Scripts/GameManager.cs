ba7b7d3941cc6c584eaf233e4cc7995cab3d48ea
DESKTOP-OUJS0N1\John <johnnytowwez@gmail.com>
2020-12-08 10:15:46 -0500

    All Enemys attack and take damage

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    //public Transform platformGenerator;
+    //private Vector3 platformStartPoint;
+
+    public PlayerController thePlayer;
+    private Vector3 playerStartPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerStartPoint = thePlayer.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void RestartGame()
+    {
+        StartCoroutine("RestartGameCo");
+    }
+    public IEnumerator RestartGameCo()
+    {
+        //thePlayer.gameObject.SetActive(false);
+        yield return new WaitForSeconds(1f);
+        thePlayer.transform.position = playerStartPoint;
+    }
+}
